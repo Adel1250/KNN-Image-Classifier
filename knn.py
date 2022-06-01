a4fb352761c6a812035c16a767684fe062f5ec7e
Adel Ashraf <es-adel.ashraf2019@alexu.edu.eg>
2022-06-01 23:55:45 +0200

    user input

--- knn.py
+++ knn.py
@@ -19,9 +19,12 @@ digits = load_digits()
 X = digits.data
 y = digits.target
 
+# Getting the number of neighbors from the user
+neighbors = int(input("Enter the number of neighbors:"))
+
 # Training KNN Model and testing it with specific K values by getting its predictions
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
-knn = OneVsRestClassifier(KNeighborsClassifier(n_neighbors = 3))
+knn = OneVsRestClassifier(KNeighborsClassifier(n_neighbors = neighbors))
 knn.fit(X_train,y_train)
 predictions = knn.predict(X_test)
 
